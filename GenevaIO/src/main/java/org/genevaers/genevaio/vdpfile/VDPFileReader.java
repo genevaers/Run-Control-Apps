f9b0e2b5846ca15cb5f559351c013c971d637fe5
AswiniV <aswini.vadlamudi@ibm.com>
2026-07-09 22:54:49 -0500

    revert VDP changes
    Signed-off-by: AswiniV <aswini.vadlamudi@ibm.com>

--- GenevaIO/src/main/java/org/genevaers/genevaio/vdpfile/VDPFileReader.java
+++ GenevaIO/src/main/java/org/genevaers/genevaio/vdpfile/VDPFileReader.java
@@ -410,10 +410,6 @@ public class VDPFileReader{
 		VDPFormatFile vffr = new VDPFormatFile();
 		vffr.readRecord(recordReader, rec);
 		currentVMD.setFormatFile(vffr);
-		// Populate the ViewNode's OutputFile from the VDP record
-		if (currentView != null) {
-			vffr.populateComponent(currentView.getOutputFile());
-		}
 		return vffr;
 	}
 
